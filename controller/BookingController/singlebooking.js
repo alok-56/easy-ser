90cd1760f2ce0bb040ea0c0bd3a2b77bb755791a
alok-56 <alokkumar11746@gmail.com>
2023-03-18 14:05:12 +0530

    apii

--- controller/BookingController/singlebooking.js
+++ controller/BookingController/singlebooking.js
@@ -1,8 +1,9 @@
-const bookingmodel =require('../../db/schema/booking');
+const bookingmodel = require('../../db/schema/booking');
 
-const Siglebook=async(req,res)=>{
-    let data=await bookingmodel.findOne({orderId:req.params.id});
+const Siglebook = async (req, res) => {
+    let data = await bookingmodel.findOne({ _id: req.params.id });
     res.status(200).json(data);
 }
 
-module.exports=Siglebook;
+
+module.exports = Siglebook;
