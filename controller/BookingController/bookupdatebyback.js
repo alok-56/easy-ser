8d22b4b8960150e1083480149b38cb1089bb775a
alok-56 <alokkumar11746@gmail.com>
2023-04-26 02:45:02 +0530

    jakak

--- controller/BookingController/bookupdatebyback.js
+++ controller/BookingController/bookupdatebyback.js
@@ -9,40 +9,42 @@ const bookupdateback = async () => {
         var Difference_In_Time = date2.getTime() - date1;
         var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
         console.log(Difference_In_Days)
-        if (Difference_In_Days > 30) {
+        if (Difference_In_Days < 30) {
             if (data[i].status != 'cancelled') {
-                let id = data[i]._id;
-                pay = "Due"
-                let update = await fetch(`https://easy-ser.vercel.app/roombooking/updatebooking`, {
-                    method: "put",
-                    body: JSON.stringify({ id, pay }),
-                    headers: {
-                        'content-type': 'application/json'
-                    }
-                })
-                update = await update.json();
-                console.log(update)
+                sendCancelemail(data[i].email, data[i]._id);
+                ownerCancelemail(data[i].ownerEmail, data[i]._id);
+                // let id = data[i]._id;
+                // pay = "Due"
+                // let update = await fetch(`https://easy-ser.vercel.app/roombooking/updatebooking`, {
+                //     method: "put",
+                //     body: JSON.stringify({ id, pay }),
+                //     headers: {
+                //         'content-type': 'application/json'
+                //     }
+                // })
+                // update = await update.json();
+                // console.log(update)
             }
-            if (Difference_In_Days > 35) {
-                let id = data[i]._id;
-                let pay = "Due";
-                let status = "Cancelled"
-                let update = await fetch(`https://easy-ser.vercel.app/roombooking/updatebooking`, {
-                    method: "put",
-                    body: JSON.stringify({ id, status, pay }),
-                    headers: {
-                        'content-type': 'application/json'
-                    }
-                })
-                update = await update.json();
-                if (update.matchedCount > 0) {
-                    // setBookid(data[i]._id)
-                    sendCancelemail(data[i].email, data[i]._id);
-                    ownerCancelemail(data[i].ownerEmail, data[i]._id);
+            // if (Difference_In_Days > 35) {
+            //     let id = data[i]._id;
+            //     let pay = "Due";
+            //     let status = "Cancelled"
+            //     let update = await fetch(`https://easy-ser.vercel.app/roombooking/updatebooking`, {
+            //         method: "put",
+            //         body: JSON.stringify({ id, status, pay }),
+            //         headers: {
+            //             'content-type': 'application/json'
+            //         }
+            //     })
+            //     update = await update.json();
+            //     if (update.matchedCount > 0) {
+            //         // setBookid(data[i]._id)
+            //         sendCancelemail(data[i].email, data[i]._id);
+            //         ownerCancelemail(data[i].ownerEmail, data[i]._id);
 
-                    console.log(update)
-                }
-            }
+            //         console.log(update)
+            //     }
+            // }
         }
 
     }
