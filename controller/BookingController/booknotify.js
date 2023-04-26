781e053ccfb9106d49762485dbee5ae9558b80f9
alok-56 <alokkumar11746@gmail.com>
2023-04-27 04:06:38 +0530

    alok

--- controller/BookingController/booknotify.js
+++ controller/BookingController/booknotify.js
@@ -6,6 +6,7 @@ const Booknotify = async (req, res) => {
     let number = req.body.ownerNumber;
     let ownerid = req.body.sellerId;
     let bookingid = req.body.book;
+    const date = new Date()
 
     const transport = nodemailer.createTransport({
         service: "gmail",
@@ -17,17 +18,15 @@ const Booknotify = async (req, res) => {
     const mailOptions = {
         from: "easypeasy11746@gmail.com",
         to: Email,
-        subject: "hello",
-        text: `
-        Hello sir/Mam,
-
-        you have successfully booked your room..
-        Owner-id : ${ownerid} 
-        OwnerName : ${name}
-        OwnerNumber : ${number}
+        subject: date,
+        html: `<H1>Hello sir/Mam!</h1><p>You have sucessfully booked your room</p>
+        <h1>Owner Information</h1>
+        <p>Owner_id</p> : <b>${ownerid}<b>
+        <p>OwnerName<p/> : <b>${name}<b>
+        <p>OwnerNumber<p> : <b>${number}<b>
      
-        Thank you..
-        regrads Third home `
+        <h2>Thank you..</h2>
+        <h3>regrads EasyPg</h3> `
     }
     transport.sendMail(mailOptions, (err, info) => {
         if (err) {
