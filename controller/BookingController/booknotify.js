f132c8593cdacc13e40d1572f22f3c704908686a
alok-56 <alokkumar11746@gmail.com>
2023-04-27 04:17:20 +0530

    laok

--- controller/BookingController/booknotify.js
+++ controller/BookingController/booknotify.js
@@ -21,12 +21,12 @@ const Booknotify = async (req, res) => {
         subject: date,
         html: `<H1>Hello sir/Mam!</h1><p>You have sucessfully booked your room</p>
         <h1>Owner Information</h1>
-        <p>Owner_id</p> : <b>${ownerid}<b>
-        <p>OwnerName<p/> : <b>${name}<b>
-        <p>OwnerNumber<p> : <b>${number}<b>
+        <p>Owner_id</p> : <p>${ownerid}<p>
+        <p>OwnerName<p/> : <p>${name}<p>
+        <p>OwnerNumber<p> : <p>${number}<p>
      
         <h2>Thank you..</h2>
-        <h3>regrads EasyPg</h3> `
+        <h3>Team EasyPg</h3> `
     }
     transport.sendMail(mailOptions, (err, info) => {
         if (err) {
